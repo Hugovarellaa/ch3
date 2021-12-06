a73cfa7c644b9fbdc00b7de1b91e0ddef3491fc4
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-06 19:18:06 -0300

    .
    .

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,24 +1,48 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { useState } from "react";
 import Modal from "react-modal";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          amount: 2900,
+          type: "deposit",
+          category: "Dev",
+          createAt: new Date("2021-12-06 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          amount: 1200,
+          type: "withdraw",
+          category: "Casa",
+          createAt: new Date("2021-12-08 11:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
-    this.get("/transactions", () => [
-      {
-        id: 1,
-        title: "Transaction 1",
-        type: "deposit",
-        amount: 400,
-        category: "Food",
-        createAt: new Date("2021-12-04"),
-      },
-    ]);
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+    this.post("transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transaction", data);
+    });
   },
 });
 
@@ -39,9 +63,10 @@ export function App() {
       <GlobalStyle />
       <Header onOpen={handleIsModalOpen} />
       <Dashboard />
-      <Modal isOpen={isModalOpen} onRequestClose={handleIsModalClose}>
-        <h2>Cadastrar transaçào</h2>
-      </Modal>
+      <NewTransactionModal
+        onOpen={isModalOpen}
+        onRequestClose={handleIsModalClose}
+      />
     </>
   );
 }
--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -54,4 +54,39 @@ export const GlobalStyle = createGlobalStyle`
     color: inherit;
     text-decoration: none;
   }
+
+  .react-modal-overlay{
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+  .closeImg{
+    position: absolute;
+    top: 1.5rem;
+    right: 1.5rem;
+    background: transparent;
+    border: 0;
+    transition: filter .2s;
+    
+    &:hover{  
+      filter: brightness(0.5);
+    }
+  }
+
+
 `;
