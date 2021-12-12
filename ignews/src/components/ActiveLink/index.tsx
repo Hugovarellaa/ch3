cedf1fcdafebe63b43d073c2712929874eed2fb6
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-12 17:38:50 -0300

    Front-end JAMStack
    1 / 2 completed

---
+++ ignews/src/components/ActiveLink/index.tsx
@@ -0,0 +1,28 @@
+import { cloneElement, ReactElement } from "react";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+
+interface activeLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+export function ActiveLink({
+  children,
+  activeClassName,
+  ...rest
+}: activeLinkProps) {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return (
+    <>
+      <Link {...rest}>
+        {cloneElement(children, {
+          className,
+        })}
+      </Link>
+    </>
+  );
+}
--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <img src="/images/logo.svg" alt="logo" />
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg" alt="logo" />
+          </a>
+        </Link>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
