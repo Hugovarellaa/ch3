f9c806e1fc0501f5351ef2a6bd35a1163c926ed8
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-14 16:11:43 -0300

    Delete ignews directory

--- ignews/src/services/prismic.ts
+++
@@ -1,9 +0,0 @@
-import Prismic from "@prismicio/client";
-
-const endpointprismic = process.env.PRISMIC_ENDPOINT;
-const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
-
-export function GetPrismicClient(req?: unknown) {
-  const prismic = Prismic.client(endpointprismic, { req, accessToken });
-  return prismic;
-}
