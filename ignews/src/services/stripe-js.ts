599634f9215b33b6e26ccece2e1cd1dcc45b9dac
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-12-18 07:50:07 -0300

    Delete ignews directory

--- ignews/src/services/stripe-js.ts
+++
@@ -1,8 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-const chaveSecreta = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
-
-export async function getStripeJs() {
-  const stripeJs = await loadStripe(chaveSecreta);
-  return stripeJs;
-}
