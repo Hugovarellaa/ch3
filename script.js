7737782efd3253353f792b8a7fc397e8472ecb40
Hugo <hugovarellaa@gmail.com>
2022-01-08 20:53:40 -0300

    Fim da estilização e começo do Javascript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("funcionando");
+const Modal = {
+  open() {
+    document.querySelector(".modal-overlay").classList.add("active");
+  },
+  close() {
+    document.querySelector(".modal-overlay").classList.remove("active");
+  },
+};
